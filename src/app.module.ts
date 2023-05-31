1c926ec4fb1c221ae45edfb549fcb52aeb7fcd75
Param Kesarwani <kparam6162@gmail.com>
2023-06-01 00:15:36 +0530

    added .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { UserModule } from './user/user.module';
 import { MongooseModule} from '@nestjs/mongoose'
 import { MailerModule } from '@nestjs-modules/mailer';
 import { EmailController } from './email.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://kesarwaniparam6162:paroparo@cluster0.yjeae6x.mongodb.net/'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.mongo_url),
     UserModule],
   controllers: [EmailController],
 })
